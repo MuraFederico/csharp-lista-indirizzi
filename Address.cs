20b63d8ff5b236eff6a1d7c6af3041af9765e787
MuraFederico <federicomura98@gmail.com>
2022-07-05 14:23:39 +0200

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public Address(string name, string surname, string street, string city, string province, int zIP)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zIP;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public int ZIP { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿
+StreamReader addresses = File.OpenText("C:/boolean/classe 56/.Net/csharp-lista-indirizzi/addresses.csv");
+
+string  fields = addresses.ReadLine();
+Console.WriteLine(fields);
